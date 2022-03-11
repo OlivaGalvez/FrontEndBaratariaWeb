d08ebcb292da0463700041f53175756973d0f72f
OLIVA GALVEZ GARCIA <Oliva.Galvez@alu.uclm.es>
2022-03-11 20:15:43 +0100

    Algunos cambios diseño

---
+++ src/app/gestion/actividades/listado-actividades/listado-actividades.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-actividades',
+  templateUrl: './listado-actividades.component.html',
+  styleUrls: ['./listado-actividades.component.scss']
+})
+export class ListadoActividadesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/gestion/routing.ts
+++ src/app/gestion/routing.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
+import { ListadoActividadesComponent } from './actividades/listado-actividades/listado-actividades.component';
+import { NuevaActividadComponent } from './actividades/nueva-actividad/nueva-actividad.component';
 import { InicioComponent } from './inicio/inicio.component';
 
 const Routing: Routes = [
 
   { path: 'inicio', component: InicioComponent },
+  {
+    path: 'actividad/listado', component: ListadoActividadesComponent
+  },
+  {
+    path: 'actividad/nueva', component: NuevaActividadComponent
+  },
   {
     path: '',
     redirectTo: '/admin/inicio',
